ea1c123a284550c9105848e2b375e316c1d3f081
Liudong137816 <184611657@qq.com>
2024-08-24 16:20:37 +0800

    Add converation demo

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+struct Conversion
+{
+	operator float() { return 0.33; }
+};
+
 void main()
 {
-	
+	Conversion con;
+	std::cout << float(con) << std::endl;
+	std::cin.get();
 }
