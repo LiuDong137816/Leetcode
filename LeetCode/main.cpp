91e9dd9115d6bc056da361f98628374a46898b2c
Dong Liu <184611657@qq.com>
2024-09-11 08:47:02 +0800

    instantiate member value

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
+#include <string>
+
+class Example
+{
+private:
+	std::string m_Name = "Unknow";
+
+public:
+	Example()
+	{
+		std::cout << "Create Example Default" << std::endl;
+	}
+
+	Example(std::string name)
+	{
+		std::cout << "Create Example Member Instantiate" << std::endl;
+	}
+};
+
+class Entity
+{
+public:
+	Entity() = default;
+	Entity(Example example)
+		:e(example)
+	{
+		
+	}
+	const std::string& GetName() const { return m_Name; }
+
+	void PrintType()const 
+	{
+		std::cout << "Entity" << std::endl;
+	}
+
+private:
+	Example e;
+	Entity* m_Parent;
+	std::string m_Name = "123";
+};
+
+void Print(const Entity& e)
+{
+	e.PrintType();
+}
+
+
 
 void main()
 {
-	
+	Example e("uu");
+	Entity entity(e);
+	//std::string str = e.GetName();
+	//std::cout << str << std::endl;
+	/*const char* val = "123";
+	std::cout << &val << std::endl;*/
+	/*const int* p = new int;
+	int a = 21;
+	p = &a;
+	std::cout << *p << std::endl;*/
+
+	/*int x = 4;
+	auto f = [=]() mutable
+		{
+			x++;
+			std::cout << x << std::endl;
+		};
+	f();
+
+	std::cout << x << std::endl;*/
+	/*Entity *e = nullptr;
+	e->PrintType();*/
+	//std::cout << e->GetName();
+	std::cin.get();
 }
